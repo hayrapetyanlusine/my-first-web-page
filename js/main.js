1ef11ca65f760757b10b6c201f685671fbeb8d31
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2022-03-21 21:17:25 +0400

    changed navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("Hi");
+window.addEventListener("DOMContentLoaded", () => {
+    const mode = document.querySelector(".mode-svg");
+
+    mode.addEventListener("click", function() {
+        document.body.classList.toggle("light");
+    });
+});
