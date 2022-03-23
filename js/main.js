f21f1dab14e21243e10e820c23a6bdc0ecaf5578
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2022-03-23 20:24:55 +0400

    add mobile menu and footer

--- js/main.js
+++ js/main.js
@@ -4,4 +4,31 @@ window.addEventListener("DOMContentLoaded", () => {
     mode.addEventListener("click", function() {
         document.body.classList.toggle("light");
     });
+
+    //mobile menu
+    const menuBtn = document.querySelector(".burger-menu");
+    const menu = document.querySelector("#menu");
+
+    menuBtn.addEventListener("click", function () {
+        menu.classList.toggle("active-nav");
+    });
+
+    // scroll up
+    const upBtn = document.querySelector(".btn-up");
+
+    function docUp() {
+        if(window.scrollY >= 600) {
+            upBtn.classList.add("showBtn");
+        } else{
+            upBtn.classList.remove("showBtn");
+        }
+    }
+
+    window.addEventListener("scroll", docUp);
+
+    upBtn.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    //
 });
